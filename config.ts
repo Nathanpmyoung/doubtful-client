02fbd0260fa3e4202d18cc39a01b7ecdd7267d31
Joe Reeve <joe.reeve@amplitude.com>
2022-09-11 20:22:32 +0100

    Update config to point to api

--- config.ts
+++ config.ts
@@ -1,17 +1,17 @@
 const configs = {
-    development: {
-        apiUrl: 'https://api.doubtful.app',
-        origin: 'https://doubtful.app/api',
-    },
-    test: {
-        apiUrl: 'http://localhost:3000',
-        origin: 'http://localhost:3001/api',
-    },
-    production: {
-        apiUrl: 'http://localhost:3000',
-        origin: 'http://localhost:3001/api',
-    },
+  development: {
+    apiUrl: "http://localhost:3000",
+    origin: "http://localhost:3001/api",
+  },
+  test: {
+    apiUrl: "http://localhost:3000",
+    origin: "http://localhost:3001/api",
+  },
+  production: {
+    apiUrl: "https://api.doubtful.app",
+    origin: "https://doubtful.app/api",
+  },
 };
 
-const environment = process.env.NODE_ENV || 'development';
+const environment = process.env.NODE_ENV || "development";
 export const config = configs[environment];
